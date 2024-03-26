8986e4c7ce42b419c8ff2212840c445eb499e82d
Andrei Popov <a.popov@paysend.com>
2024-03-26 09:08:50 +0100

    Added `list` command to show all currently synced calendars (with count of events)

---
+++ list.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func listCalendars() {
+	db, err := openDB(".gcalsync.db")
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	fmt.Println("📋 Here's the list of calendars you are syncing:")
+
+	rows, err := db.Query("SELECT account_name, calendar_id, count(1) as num_events FROM blocker_events GROUP BY 1,2;")
+	if err != nil {
+		log.Fatalf("❌ Error retrieving blocker events from database: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var accountName, calendarID string
+		var numEvents int
+		if err := rows.Scan(&accountName, &calendarID, &numEvents); err != nil {
+			log.Fatalf("❌ Unable to read calendar record or no calendars defined: %v", err)
+		}
+		fmt.Printf("  👤 %s (📅 %s) - %d\n", accountName, calendarID, numEvents)
+	}
+}
